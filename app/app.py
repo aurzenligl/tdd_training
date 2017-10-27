978cd257908fe897501668903f9cc4f2dc86828d
Krzysztof Laskowski <krzysztof.laskowski@nokia.com>
2017-10-27 03:27:45 +0200

    application skeleton

---
+++ app/app.py
@@ -0,0 +1,8 @@
+import sys
+
+def main(argv=[]):
+    print("app main function")
+    return 0
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv))
---
+++ tests/test_app.py
@@ -0,0 +1,4 @@
+import app
+
+def test_app():
+    assert app.main() == 0
