3888ec7c7b84d9d2fe796dd4bc9983dd0fbc11aa
Krzysztof Laskowski <krzysztof.laskowski@nokia.com>
2017-10-29 22:38:38 +0100

    iteration test

--- tests/test_level.py
+++ tests/test_level.py
@@ -23,3 +23,15 @@ def test_level_indexing():
     level[1,0] = W
 
     assert level[1,0] == W
+
+def test_level_iteration():
+    level = Level(3, 2, [S, W, P, S, P, W])
+
+    assert [_ for _ in level] == [
+        ((0,0), S),
+        ((1,0), W),
+        ((2,0), P),
+        ((0,1), S),
+        ((1,1), P),
+        ((2,1), W),
+    ]
