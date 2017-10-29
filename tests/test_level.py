043294a24cfd6a9590a455a6bff72b61b7335125
Krzysztof Laskowski <krzysztof.laskowski@nokia.com>
2017-10-29 22:05:13 +0100

    indexing test added: failing

--- tests/test_level.py
+++ tests/test_level.py
@@ -2,11 +2,15 @@ from app.level import SquareType, Level
 
 S = SquareType.SPACE
 W = SquareType.WALL
+P = SquareType.PLAYER
 
 def test_level_empty():
     level = Level(0, 0, [])
     assert level.size() == (0, 0)
 
 def test_level_filled():
-    level = Level(2, 3, [S, W, S, W, W, S])
-    assert level.size() == (2, 3)
+    level = Level(3, 2, [S, W, S, S, S, P])
+    assert level.size() == (3, 2)
+    assert level(0, 0) == S
+    assert level(1, 0) == W
+    assert level(2, 1) == P
