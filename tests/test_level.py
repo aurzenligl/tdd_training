a39a10e323f56ae934bc21f1afc2c178d580d159
Krzysztof Laskowski <krzysztof.laskowski@nokia.com>
2017-10-29 22:02:21 +0100

    test filled level

--- tests/test_level.py
+++ tests/test_level.py
@@ -1,5 +1,12 @@
 from app.level import SquareType, Level
 
+S = SquareType.SPACE
+W = SquareType.WALL
+
 def test_level_empty():
     level = Level(0, 0, [])
     assert level.size() == (0, 0)
+
+def test_level_filled():
+    level = Level(2, 3, [S, W, S, W, W, S])
+    assert level.size() == (2, 3)
