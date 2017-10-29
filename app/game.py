ed655fa9d522f19a09c381249681bc4044c35ff9
Krzysztof Laskowski <krzysztof.laskowski@nokia.com>
2017-10-30 00:38:35 +0100

    test passes

--- app/game.py
+++ app/game.py
@@ -2,11 +2,11 @@ class Game():
     """Game logic, knows how to move and what to render"""
 
     def __init__(self, level):
-        pass
+        self.level = level
 
     def size(self):
         """Returns cols and rows tuple"""
-        pass
+        return self.level.size()
 
     def on_render(self, drawer):
         """Draws all game objects"""
