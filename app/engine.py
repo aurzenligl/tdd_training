889a757bc24a001a635c012dcc4da38d6abd3e76
Krzysztof Laskowski <krzysztof.laskowski@nokia.com>
2017-10-30 00:24:48 +0100

    engine and game APIs

---
+++ app/engine.py
@@ -0,0 +1,17 @@
+class Engine():
+    """Manages game resources"""
+
+    def __init__(self, game):
+        pass
+
+    def run(self, game):
+        """Initializes screen and runs game main loop."""
+        pass
+
+    def _render(self):
+        """Renders new game frame"""
+        pass
+
+    def _keypress(self, key):
+        """Reacts on any key press"""
+        pass
---
+++ app/game.py
@@ -0,0 +1,17 @@
+class Game():
+    """Game logic, knows how to move and what to render"""
+
+    def __init__(self, level):
+        pass
+
+    def size(self):
+        """Returns cols and rows tuple"""
+        pass
+
+    def on_render(self, drawer):
+        """Draws all game objects"""
+        pass
+
+    def on_move(self, direction):
+        """Reacts to movement"""
+        pass
