a7646c1fc96a7565b3715198d061ead71c4a8526
Krzysztof Laskowski <krzysztof.laskowski@nokia.com>
2017-11-01 20:47:04 +0100

    large level hardcoded

--- app/hardcoded_level.py
+++ app/hardcoded_level.py
@@ -6,10 +6,23 @@ B = SquareType.BOX
 G = SquareType.GOAL
 
 hardcoded_level = [
-    W, W, W, W, W,
-    W, S, B, S, W,
-    W, W, W, W, W,
+    S, S, S, S, S, S, S, S, S, S, S, S, S, S, S, S, S, S, S, S,
+    S, S, S, S, S, S, S, S, S, S, S, S, S, S, S, S, S, S, S, S,
+    S, S, S, S, S, S, S, S, S, S, S, S, S, S, S, S, S, S, S, S,
+    S, S, S, S, S, S, S, S, S, S, S, W, W, W, W, W, S, S, S, S,
+    S, S, S, S, S, W, W, W, W, W, W, W, S, S, S, W, W, S, S, S,
+    S, S, S, S, W, W, S, W, S, S, W, W, S, B, B, S, W, S, S, S,
+    S, S, S, S, W, S, S, S, S, B, S, S, S, S, S, S, W, S, S, S,
+    S, S, S, S, W, S, S, B, S, S, W, W, W, S, S, S, W, S, S, S,
+    S, S, S, S, W, W, W, S, W, W, W, W, W, B, W, W, W, S, S, S,
+    S, S, S, S, W, S, B, S, S, W, W, W, S, G, G, W, S, S, S, S,
+    S, S, S, S, W, S, B, S, B, S, B, S, G, G, G, W, S, S, S, S,
+    S, S, S, S, W, S, S, S, S, W, W, W, G, G, G, W, S, S, S, S,
+    S, S, S, S, W, S, B, B, S, W, S, W, G, G, G, W, S, S, S, S,
+    S, S, S, S, W, S, S, W, W, W, S, W, W, W, W, W, S, S, S, S,
+    S, S, S, S, W, W, W, W, S, S, S, S, S, S, S, S, S, S, S, S,
+    S, S, S, S, S, S, S, S, S, S, S, S, S, S, S, S, S, S, S, S,
 ]
 
 def get_level():
-    return Level(5, 3, hardcoded_level)
+    return Level(20, 16, hardcoded_level)
