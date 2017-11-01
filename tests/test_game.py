8be075cdc6eb7484ff670aacad930617dbc4a52e
Krzysztof Laskowski <krzysztof.laskowski@nokia.com>
2017-11-02 00:54:26 +0100

    failing test: other directions

--- tests/test_game.py
+++ tests/test_game.py
@@ -41,10 +41,21 @@ def test_game_rendering(level):
     drawer.square.assert_any_call((1,2), S)
     drawer.square.assert_any_call((2,1), B)
 
-def test_game_movement_illegal(level):
+@pytest.mark.parametrize("movement", [
+    Direction.UP,
+    Direction.DOWN,
+    Direction.LEFT,
+    Direction.RIGHT
+])
+def test_game_movement_illegal(level, movement):
+    level = Level((3,3), player=(1,1), tiles=[
+        W, W, W,
+        W, S, W,
+        W, W, W,
+    ])
     game = Game(level)
 
     with pytest.raises(MoveError):
-        game.on_move(Direction.LEFT)
+        game.on_move(movement)
 
     assert level.player == (1, 1)
