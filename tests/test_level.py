87e190bd723374116ad94a1443ef06e5d2b076f5
Krzysztof Laskowski <krzysztof.laskowski@nokia.com>
2017-11-01 22:55:47 +0100

    failing test: player set not on space or goal

--- tests/test_level.py
+++ tests/test_level.py
@@ -4,10 +4,11 @@ from app.level import Tile, Level
 S = Tile.SPACE
 W = Tile.WALL
 B = Tile.BOX
+G = Tile.GOAL
 
 def make_example_level():
     tiles = [S, W, B,
-             S, B, W]
+             S, B, G]
     return Level((3, 2), tiles, (0, 1))
 
 def test_level_filled():
@@ -34,7 +35,7 @@ def test_level_iteration():
         ((2,0), B),
         ((0,1), S),
         ((1,1), B),
-        ((2,1), W),
+        ((2,1), G),
     ]
 
 def test_level_player_position():
@@ -45,9 +46,11 @@ def test_level_player_position():
 def test_level_player_position_setting():
     level = make_example_level()
 
-    level.player = (0, 0)
-
+    level.player = (0, 0)  # other space
     assert level.player == (0, 0)
+
+    level.player = (2, 1)  # goal
+    assert level.player == (2, 1)
 
 def test_level_error_too_few_squaretypes():
     with pytest.raises(ValueError) as e:
@@ -73,3 +76,8 @@ def test_level_error_player_not_on_space():
     with pytest.raises(ValueError) as e:
         Level((1, 1), [W], (0,0))
     assert str(e.value) == "expected player on empty space"
+
+def test_level_error_setting_player_not_on_space():
+    with pytest.raises(ValueError) as e:
+        Level((2, 1), [S, W], (0, 0)).player = (1, 0)
+    assert str(e.value) == "expected player on empty space or goal"
