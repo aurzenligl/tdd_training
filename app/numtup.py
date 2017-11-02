4e1bd7925c3e1c18e3a3c7aceb9eed1a46958bf4
Krzysztof Laskowski <krzysztof.laskowski@nokia.com>
2017-11-03 00:31:05 +0100

    numtup operators return numtup instead of ordinary tuples, numtup tests parametrized

--- app/numtup.py
+++ app/numtup.py
@@ -2,11 +2,11 @@ class numtup(tuple):
     def __add__(self, other):
         if isinstance(other, tuple):
             assert len(self) == len(other)
-            return tuple(lhs + rhs for lhs, rhs in zip(self, other))
-        return tuple(lhs + other for lhs in self)
+            return numtup(lhs + rhs for lhs, rhs in zip(self, other))
+        return numtup(lhs + other for lhs in self)
 
     def __mul__(self, other):
         if isinstance(other, tuple):
             assert len(self) == len(other)
-            return tuple(lhs * rhs for lhs, rhs in zip(self, other))
-        return tuple(lhs * other for lhs in self)
+            return numtup(lhs * rhs for lhs, rhs in zip(self, other))
+        return numtup(lhs * other for lhs in self)
--- tests/test_numtup.py
+++ tests/test_numtup.py
@@ -1,13 +1,22 @@
+import pytest
 from app.numtup import numtup
 
-def test_numtup_add():
-    assert numtup((1, 2)) + 3 == (4, 5)
-    assert numtup((1, 2)) + (1, 5) == (2, 7)
-    assert numtup((1, 2, 3)) + 1 == (2, 3, 4)
-    assert numtup((1, 2, 3)) + (3, 2, 1) == (4, 4, 4)
+@pytest.mark.parametrize('expr,res', [
+    (numtup((1, 2)) + 3, (4, 5)),
+    (numtup((1, 2)) + (1, 5), (2, 7)),
+    (numtup((1, 2, 3)) + 1, (2, 3, 4)),
+    (numtup((1, 2, 3)) + (3, 2, 1), (4, 4, 4)),
+])
+def test_numtup_add(expr, res):
+    assert expr == res
+    assert isinstance(expr, numtup)
 
-def test_numtup_mul():
-    assert numtup((1, 2)) * 3 == (3, 6)
-    assert numtup((1, 2)) * (1, 5) == (1, 10)
-    assert numtup((1, 2, 3)) * 2 == (2, 4, 6)
-    assert numtup((1, 2, 3)) * (3, 2, 1) == (3, 4, 3)
+@pytest.mark.parametrize('expr,res', [
+    (numtup((1, 2)) * 3, (3, 6)),
+    (numtup((1, 2)) * (1, 5), (1, 10)),
+    (numtup((1, 2, 3)) * 2, (2, 4, 6)),
+    (numtup((1, 2, 3)) * (3, 2, 1), (3, 4, 3)),
+])
+def test_numtup_mul(expr, res):
+    assert expr == res
+    assert isinstance(expr, numtup)
