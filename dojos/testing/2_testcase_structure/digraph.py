2af375213568272d4de17195405fc45ce7878a53
Krzysztof Laskowski <krzysztof.laskowski@nokia.com>
2017-11-19 00:49:45 +0100

    digraph with broken test

---
+++ dojos/testing/2_testcase_structure/digraph.py
@@ -0,0 +1,89 @@
+'''
+Let's handle a bigger chunk of code.
+How would you test a set of tightly connected classes?
+'''
+
+class Digraph(object):
+    '''Directed graph with labeled nodes and edges.'''
+
+    def __init__(self):
+        self.nodes = []
+        self.edges = []
+        self.heads = {}
+        self.tails = {}
+
+    def add_node(self, label):
+        node = Node(self, label)
+        self.nodes.append(node)
+        return node
+
+    def add_edge(self, head, tail, label):
+        edge = Edge(head, tail, label)
+        self.edges.append(edge)
+        def append(mapping, key, value):
+            values = mapping.setdefault(key, [])
+            values.append(value)
+        append(self.heads, head, edge)
+        append(self.tails, tail, edge)
+        return edge
+
+    def remove_node(self, node):
+        edges = self.heads[node] + self.tails[node]
+        map(self.remove_edge, edges)
+        self.nodes.remove(node)
+
+    def remove_edge(self, edge):
+        self.edges.remove(edge)
+        self.heads[edge.head].remove(edge)
+        self.tails[edge.tail].remove(edge)
+
+    def node(self, label):
+        return next(n for n in self.nodes if n.label == label)
+
+    def edge(self, label):
+        return next(e for e in self.edges if e.label == label)
+
+class Node(object):
+    def __init__(self, graph, label):
+        self._graph = graph
+        self.label = label
+
+    @property
+    def head_nodes(self):
+        return [e.head for e in self.head_edges]
+
+    @property
+    def head_edges(self):
+        return self._graph.tails.get(self, [])
+
+    @property
+    def tail_nodes(self):
+        return [e.tail for e in self.tail_edges]
+
+    @property
+    def tail_edges(self):
+        return self._graph.heads.get(self, [])
+
+    def __repr__(self):
+        return '<Node %s>' % self.label
+
+class Edge(object):
+    def __init__(self, head, tail, label):
+        self._head = head
+        self._tail = tail
+        self.label = label
+
+    @property
+    def head(self):
+        return self._head
+
+    @property
+    def tail(self):
+        return self._tail
+
+    @property
+    def nodes(self):
+        return self.head, self.tail
+
+    def __repr__(self):
+        return '<Edge %s>' % self.label
---
+++ dojos/testing/2_testcase_structure/test_digraph_broken.py
@@ -0,0 +1,64 @@
+import pytest
+from digraph import Digraph
+
+'''
+How about like this?
+'''
+
+def test_digraph():
+    g = Digraph()
+    n1 = g.add_node('N1')
+    n2 = g.add_node('N2')
+    n3 = g.add_node('N3')
+    e1 = g.add_edge(n1, n2, 'E1')
+    e2 = g.add_edge(n2, n3, 'E2')
+    e3 = g.add_edge(n1, n3, 'E3')
+    e4 = g.add_edge(n1, n3, 'E4')
+
+    assert len(g.nodes) == 3
+    assert len(g.edges) == 4
+    assert g.edge('E1').nodes == (n1, n2)
+    assert g.edge('E2').nodes == (n2, n3)
+    assert g.edge('E3').nodes == (n1, n3)
+    assert g.edge('E4').nodes == (n1, n3)
+    assert g.node('N1').head_nodes == []
+    assert g.node('N1').head_edges == []
+    assert g.node('N1').tail_nodes == [n2, n3, n3]
+    assert g.node('N1').tail_edges == [e1, e3, e4]
+    assert g.node('N2').head_nodes == [n1]
+    assert g.node('N2').head_edges == [e1]
+    assert g.node('N2').tail_nodes == [n3]
+    assert g.node('N2').tail_edges == [e2]
+    assert g.node('N3').head_nodes == [n2, n1, n1]
+    assert g.node('N3').head_edges == [e2, e3, e4]
+    assert g.node('N3').tail_nodes == []
+    assert g.node('N3').tail_edges == []
+
+    g.remove_node(n2)
+
+    assert len(g.nodes) == 2
+    assert len(g.edges) == 2
+    assert g.edge('E3').nodes == (n1, n3)
+    assert g.edge('E4').nodes == (n1, n3)
+    assert g.node('N1').head_nodes == []
+    assert g.node('N1').head_edges == []
+    assert g.node('N1').tail_nodes == [n3, n3]
+    assert g.node('N1').tail_edges == [e3, e4]
+    assert g.node('N3').head_nodes == [n1, n1]
+    assert g.node('N3').head_edges == [e3, e4]
+    assert g.node('N3').tail_nodes == []
+    assert g.node('N3').tail_edges == []
+
+    g.remove_edge(e3)
+
+    assert len(g.nodes) == 2
+    assert len(g.edges) == 1
+    assert g.edge('E4').nodes == (n1, n3)
+    assert g.node('N1').head_nodes == []
+    assert g.node('N1').head_edges == []
+    assert g.node('N1').tail_nodes == [n3]
+    assert g.node('N1').tail_edges == [e4]
+    assert g.node('N3').head_nodes == [n1]
+    assert g.node('N3').head_edges == [e4]
+    assert g.node('N3').tail_nodes == []
+    assert g.node('N3').tail_edges == []
