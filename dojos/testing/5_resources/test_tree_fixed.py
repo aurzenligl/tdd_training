c9fe755711e18de8b722118f710e021d6cb5e06a
Krzysztof Laskowski <krzysztof.laskowski@nokia.com>
2017-11-23 01:18:34 +0100

    explanation added

--- dojos/testing/5_resources/test_tree_fixed.py
+++ dojos/testing/5_resources/test_tree_fixed.py
@@ -3,6 +3,34 @@ from tree import tree
 
 class TestTree(object):
 
+    '''
+    When it comes to filesystem, we have two options:
+    - mock away calls related to filesystem and make them return file contents and stats,
+    - prepare a tempdir with proper directory structure and ability to alter it.
+
+    In this case, with Python's ease of filesystem manipulation,
+    it's a crime to mock away calls to listdir/stat/isdir, etc.
+    Such mocking in this case is going to make your tests unintelligible and rigid,
+    because tree uses filesystem copiously and you don't want a change in implementation
+    to render your carefully prepared mocks useless.
+
+    Fixture tmpdir creates a temporary dir in /tmp (on unix-likes), which is unique per
+    testcase and gets recycled during consecutive pytest run. You don't have to clean
+    this directory during testcase, framework will do it for you.
+
+    Additional bonus to solving resource testing problems that way is that we have a
+    guarantee that code integrates with resource (filesystem, network, database),
+    instead of a vague promise that it does, which mocks give (we may make mistakes
+    by defining mocks). And we can debug resources using external tools, like
+    shell (filesystem), wireshark (network) or database (other database applications).
+    '''
+
+    '''
+    When it comes to stdout printing, we have two options again:
+    - do away with printing and return string instead,
+    - keep printing and use facility to redirect them to string which you can assert.
+    Again, in Python second option is viable, we can use capsys fixture which does just this.
+    '''
     def test_file(self, tmpdir, capsys):
         root = tmpdir.join('foo')
         root.write('', ensure=True)
@@ -75,6 +103,11 @@ class TestTree(object):
             '    |-- baz\n'
             '    `-- xyz\n') % str(root)
 
+    '''
+    We can prepare arbitrarily large "integration" test data in fixture
+    and use it in multiple tests. After test execution (failure?) we may
+    enter test directory and check state of this directory structure.
+    '''
     @pytest.fixture
     def testdir(self, tmpdir):
         root = tmpdir.join('root').ensure_dir()
