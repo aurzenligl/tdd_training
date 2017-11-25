8f00c1a5081f7dfdb0aaae8b1bfc3f5e57fba337
Krzysztof Laskowski <krzysztof.laskowski@nokia.com>
2017-11-25 03:54:05 +0100

    refactoring dojo explanation

--- dojos/refactoring/tsort.py
+++ dojos/refactoring/tsort.py
@@ -1,3 +1,14 @@
+'''
+You see this function in your codebase.
+Maybe you want to change/extend its functionality, maybe you want to fix a bug.
+You don't know what it does and don't understand implementation.
+
+Luckily, you've got a suite of tests which serve as the only readable documentation of what this does.
+You can run those and see that they pass.
+
+How would you approach refactoring this function, so as to make it readable?
+'''
+
 def tsort(depvects):
     kdepvects = []
     idx = 0
